0cc50946ec356bb1aac91b5fa5e6d8c4bf2cef95
Samantha Antonious <saantoni@ucsd.edu>
2017-02-16 12:44:00 -0800

    ajax lab! :)

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,29 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, callback)
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function callback(elem) {
+	console.log(elem);
+
+	var projectHTML = '<img class="detailsImage" src="' + elem["image"] +'">' +
+	elem["title"] + "</h5>" + "<h6>" + elem["date"] + "</h6>" + "<p>" +
+	elem["summary"] + "</p";
+
+	$("#project" + elem["id"] + ".details").html(projectHTML);
+}
+
+function colorAJAX(elem){
+	var colors = elem.colors.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
 
 /*
@@ -35,4 +57,6 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette/", colorAJAX);
 }
